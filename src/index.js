8b8ee47dfca927117000412f84d23fc77697e45b
David Resende <davidresende18@gmail.com>
2022-03-16 23:22:28 -0400

    token is only letters and numbers now

--- src/index.js
+++ src/index.js
@@ -14,15 +14,9 @@ const { Schema } = mongoose;
 // Token Generator
 const allCapsAlpha = [..."ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
 const allLowerAlpha = [..."abcdefghijklmnopqrstuvwxyz"];
-const allUniqueChars = [..."~!@#$%^&*()_+-=[]\\{}|;:'\",./<>?"];
 const allNumbers = [..."0123456789"];
 
-const base = [
-  ...allCapsAlpha,
-  ...allNumbers,
-  ...allLowerAlpha,
-  ...allUniqueChars,
-];
+const base = [...allCapsAlpha, ...allNumbers, ...allLowerAlpha];
 
 const generator = (base, len) => {
   return [...Array(len)]
